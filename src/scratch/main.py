6ab43c2eb6a677be5d940bfa5473079ce818e25f
jjjuuuun <kjy961226@naver.com>
2023-07-17 16:05:11 +0900

    Refactor: Refactor for CI

---
+++ src/scratch/main.py
@@ -0,0 +1,162 @@
+# Python built-in modules
+import os
+import io
+import base64
+import uuid
+from datetime import datetime
+
+# Pytorch
+import torch
+from torch import cuda
+
+# Backend
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import JSONResponse
+
+from pydantic import BaseModel
+
+# Other modules
+import numpy as np
+from pytz import timezone
+
+# Built-in modules
+from src.scratch.gpt3_api import get_description
+from src.scratch.gcp.bigquery import BigQueryLogger
+from src.scratch.gcp.cloud_storage import GCSUploader
+from src.scratch.gcp.error import ErrorReporter
+from src.scratch.model import AlbumModel
+from src.scratch.utils import load_yaml
+
+
+# Load config
+gcp_config = load_yaml(os.path.join("src/scratch/config", "private.yaml"), "gcp")
+public_config = load_yaml(os.path.join("src/scratch/config", "public.yaml"))
+
+# Start fastapi
+app = FastAPI()
+
+bigquery_logger = BigQueryLogger(gcp_config)
+gcs_uploader = GCSUploader(gcp_config)
+error_reporter = ErrorReporter(gcp_config)
+
+device = "cuda" if cuda.is_available() else "cpu"
+
+# Generate a unique ID for this request
+request_id = str(uuid.uuid4())
+
+
+def load_model():
+    model = AlbumModel(public_config["model"], public_config["language"], device)
+    return model
+
+
+# Album input Schema
+class AlbumInput(BaseModel):
+    song_names: str
+    artist_name: str
+    genre: str
+    album_name: str
+    lyric: str
+
+
+# Review input Schema
+class ReviewInput(BaseModel):
+    rating: int
+    comment: str
+
+
+# REST API - Post ~/generate_cover
+@app.post("/generate_cover")
+async def generate_cover(album: AlbumInput, model: AlbumModel = Depends(load_model)):
+    images = []
+    urls = []
+
+    summarization = get_description(
+        album.lyric,
+        album.artist_name,
+        album.album_name,
+        album.song_names,
+    )
+
+    seeds = np.random.randint(
+        public_config["generate"]["max_seed"], size=public_config["generate"]["n_gen"]
+    )
+
+    for i, seed in enumerate(seeds):
+        generator = torch.Generator(device=device).manual_seed(int(seed))
+
+        # Generate Images
+        with torch.no_grad():
+            image = model.pipeline(
+                prompt=f"A photo of a {album.genre} album cover with a {summarization} atmosphere visualized.",
+                num_inference_steps=public_config["generate"]["inference_step"],
+                generator=generator,
+            ).images[0]
+
+        image = image.resize(
+            (public_config["generate"]["height"], public_config["generate"]["width"])
+        )
+
+        # Convert to base64-encoded string
+        byte_arr = io.BytesIO()
+        image.save(byte_arr, format=public_config["generate"]["save_format"])
+        byte_arr = byte_arr.getvalue()
+        base64_str = base64.b64encode(byte_arr).decode()
+
+        urls.append(
+            [
+                byte_arr,
+                f"{request_id}_image_{i}.{public_config['generate']['save_format']}",
+            ]
+        )
+
+        images.append(base64_str)
+
+    # Upload to GCS
+    image_urls = gcs_uploader.save_image_to_gcs(urls)
+
+    # Log to BigQuery
+    album_log = {
+        "request_id": request_id,
+        "request_time": datetime.utcnow()
+        .astimezone(timezone("Asia/Seoul"))
+        .isoformat(),
+        "song_names": album.song_names,
+        "artist_name": album.artist_name,
+        "genre": album.genre,
+        "album_name": album.album_name,
+        "lyric": album.lyric,
+        "summarization": summarization,
+        "image_urls": image_urls,
+        "language": public_config["language"],
+    }
+    bigquery_logger.log(album_log, "user_album")
+
+    return {"images": images}
+
+
+# REST API - Post ~/review
+@app.post("/review")
+async def review(review: ReviewInput):
+    # Log to BigQuery
+    review_log = {
+        "request_id": request_id,
+        "request_time": datetime.utcnow()
+        .astimezone(timezone("Asia/Seoul"))
+        .isoformat(),
+        "rating": review.rating,
+        "comment": review.comment,
+        "language": public_config["language"],
+    }
+
+    bigquery_logger.log(review_log, "user_review")
+
+    return review
+
+
+# Exception handling using google cloud
+@app.exception_handler(Exception)
+async def handle_exceptions(request: Request, exc: Exception):
+    error_reporter.python_error()
+
+    return JSONResponse(status_code=500, content={"message": "Internal Server Error"})
---
+++ src/scratch/streamlit_frontend.py
@@ -0,0 +1,148 @@
+# Python built-in modules
+import os
+import base64
+import io
+
+# Frontend
+import streamlit as st
+
+# Other modules
+import pandas as pd
+import requests
+from PIL import Image
+
+# Built-in modules
+try:
+    from src.scratch.utils import load_yaml
+except:
+    from utils import load_yaml
+
+
+def main():
+    # Load config
+    request_config = load_yaml(
+        os.path.join("src/scratch/config", "private.yaml"), "request"
+    )
+    public_config = load_yaml(os.path.join("src/scratch/config", "public.yaml"))
+    language = public_config["language"]
+    translation_config = load_yaml(
+        os.path.join("src/scratch/config", "translation.yaml"), language
+    )
+
+    # Frontend
+    st.title(translation_config["title"])
+
+    # 1. Input album information
+    st.header(translation_config["album_info"])
+
+    song_names = st.text_input(
+        translation_config["song_names"]["text"],
+        placeholder=translation_config["song_names"]["placeholder"],
+    )
+    artist_name = st.text_input(
+        translation_config["artist_name"]["text"],
+        placeholder=translation_config["artist_name"]["placeholder"],
+    )
+    genre = st.selectbox(
+        translation_config["genre"]["text"],
+        translation_config["genre"]["list"],
+    )
+    album_name = st.text_input(
+        translation_config["album_name"]["text"],
+        placeholder=translation_config["album_name"]["placeholder"],
+    )
+    lyric = st.text_area(
+        translation_config["lyric"]["text"],
+        placeholder=translation_config["lyric"]["placeholder"],
+    )
+
+    info = {
+        translation_config["info"][0]: song_names,
+        translation_config["info"][1]: artist_name,
+        translation_config["info"][2]: genre,
+        translation_config["info"][3]: album_name,
+        translation_config["info"][4]: lyric,
+    }
+
+    # 2. Show info dataframe
+    info_df = pd.DataFrame(
+        list(info.values()),
+        index=list(info.keys()),
+        columns=[translation_config["dataframe"]["col"]],
+    )
+    info_table = st.dataframe(info_df, use_container_width=True)
+
+    # 3. Inference
+    request_info = {
+        request_config["info"][0]: song_names,
+        request_config["info"][1]: artist_name,
+        request_config["info"][2]: genre,
+        request_config["info"][3]: album_name,
+        request_config["info"][4]: lyric,
+    }
+
+    st.header(translation_config["inference"]["header"])
+    gen_button = st.button(
+        translation_config["inference"]["button_message"], use_container_width=True
+    )
+
+    # Set 'img' variable to session state
+    if "img" not in st.session_state:
+        st.session_state.img = ["", "", "", ""]
+
+    # Create 2x2 grid
+    col1, col2 = st.columns(2)
+    cols = [col1, col2, col1, col2]
+
+    if gen_button:
+        with st.spinner(translation_config["inference"]["wait_message"]):
+            # Call the FastAPI server to generate the album cover
+            response = requests.post(request_config["gen_address"], json=request_info)
+            if response.status_code == 200:
+                images = response.json()["images"]
+
+                # Assign images to the cells in the grid
+                for i, image in enumerate(images):
+                    img_data = base64.b64decode(image)
+                    img = Image.open(io.BytesIO(img_data))
+                    st.session_state.img[i] = img
+                    cols[i].image(img, width=300)
+            else:
+                st.error(translation_config["inference"]["fail_meesage"]["generate"])
+    else:
+        if st.session_state.img == ["", "", "", ""]:
+            for i in range(len(cols)):
+                cols[i].empty()
+        else:
+            for i in range(len(cols)):
+                cols[i].image(st.session_state.img[i], width=300)
+
+    # 4. Review
+    with st.expander(translation_config["review"]["expander"]):
+        review_rating = st.select_slider(
+            translation_config["review"]["rating"], options=range(1, 6), value=5
+        )
+        st.markdown(
+            "<h5 style='text-align: center;'>"
+            + "❤️" * review_rating
+            + "🖤" * (5 - review_rating)
+            + "</h5>",
+            unsafe_allow_html=True,
+        )
+        review_text = st.text_input(
+            translation_config["review"]["review_text"]["text"],
+            placeholder=translation_config["review"]["review_text"]["placeholder"],
+            label_visibility="hidden",
+        )
+        review_btn = st.button(translation_config["review"]["button"])
+        if review_btn:
+            review = {"rating": review_rating, "comment": review_text}
+            response = requests.post(request_config["review_address"], json=review)
+            if response.status_code == 200:
+                st.write(translation_config["review"]["message"]["success"])
+            else:
+                st.error(translation_config["review"]["message"]["fail"])
+
+
+if __name__ == "__main__":
+    main()

a8a0f838c8ceb55b3e3f45c0213bf9dbe28330d8
seungki1011 <kimseungki1011@gmail.com>
2023-07-30 17:37:16 +0900

    Refactor: Change directories, Add worker for sdxl
    Github issue #85

--- src/scratch/gcp/cloud_storage.py
+++ src/scratch/gcp/cloud_storage.py
@@ -12,21 +12,15 @@ from datetime import datetime, timedelta
 class GCSUploader:
     def __init__(self, gcp_config: dict):
         credentials_path = gcp_config["credentials_path"]
-        self.default_bucket_name = gcp_config["cloud_storage"]["bucket_name"]
+        self.bucket_name = gcp_config["cloud_storage"]["bucket_name"]
 
         credentials = service_account.Credentials.from_service_account_file(
             credentials_path
         )
         self.client = storage.Client(credentials=credentials)
 
-    def upload_blob(
-        self,
-        source_file_data: bytes,
-        destination_blob_name: str,
-        bucket_name: str = None,
-    ) -> str:
-        bucket_name = bucket_name if bucket_name else self.default_bucket_name
-        bucket = self.client.get_bucket(bucket_name)
+    def upload_blob(self, source_file_data: bytes, destination_blob_name: str) -> str:
+        bucket = self.client.get_bucket(self.bucket_name)
         blob = bucket.blob(destination_blob_name)
 
         file_obj = io.BytesIO(source_file_data)
@@ -42,14 +36,10 @@ class GCSUploader:
         return user_url
 
     # Uploads image to GCS and returns the URL
-    def save_image_to_gcs(
-        self,
-        urls: list,
-        bucket_name: str = None,
-    ) -> str:
+    def save_image_to_gcs(self, urls: list) -> str:
         image_urls = []
         for i, (byte_arr, url_name) in enumerate(urls):
-            url = self.upload_blob(byte_arr, url_name, bucket_name)
+            url = self.upload_blob(byte_arr, url_name)
             image_urls.append(url)
 
         return image_urls
